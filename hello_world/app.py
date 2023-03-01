dfe8adaf643049525bc96b4bf88d498b9c8a0563
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-03-01 20:11:03 +0100

    add external api call

--- hello_world/app.py
+++ hello_world/app.py
@@ -5,14 +5,22 @@ dynamodb = boto3.client('dynamodb')
 table_name = 'AF_bot_table'
 results = dynamodb.scan(TableName=table_name, ProjectionExpression='apartments')
 
+msg = ""
+for res in results['Items']:
+    msg += res['apartments']['S'] + "\n"
+
 import requests
+
+r = requests.get('https://swapi.dev/api/people/1/')
+msg += (r.json()['name'])
+
 def lambda_handler(event, context):
     client = boto3.client("ses")
     subject = "test subject from lambda"
-    body = "test body from lambda"
+    body = msg
     message = {"Subject": {"Data": subject}, "Body": {"Html": {"Data": body}}}
-    # response = client.send_email(Source = "tomas.nyberg7335@gmail.com",
-    #            Destination = {"ToAddresses": ["tomas.nyberg7335@gmail.com"]}, Message = message)
+    response = client.send_email(Source = "tomas.nyberg7335@gmail.com",
+               Destination = {"ToAddresses": ["tomas.nyberg7335@gmail.com"]}, Message = message)
 
     return {
         "statusCode": 200,
